3721e8e87ee7267f08f58d10bc9c2f241580dfd1
Loup Thevenin <loup@macbook-pro-de-loup.home>
2024-12-24 16:36:59 +0100

    start mlx

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 14:22:24 by ltheveni          #+#    #+#             */
-/*   Updated: 2024/12/23 12:27:16 by ltheveni         ###   ########.fr       */
+/*   Updated: 2024/12/24 16:32:51 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,45 @@
 
 typedef struct s_settings
 {
-	int		column;
-	int		line;
-	int		width;
-	int		height;
-	int		collectible;
-	int		exit;
-	char	**map;
-}			t_settings;
+	int			column;
+	int			line;
+	int			width;
+	int			height;
+	int			sprite_size;
+	int			collectible;
+	int			exit;
+	char		**map;
+}				t_settings;
 
-void		free_map(char **map, int size);
-char		**get_map(const char *path, int line_count, int *column_count);
-int			count_lines(const char *path);
-void		init_settings(t_settings *settings, const char *path);
-void		error_map(char *line, int fd, t_settings *settings, char *msg);
-void		count_all(char *line, int *count);
-void		check_count(int *count, t_settings *settings);
-void		check_map(t_settings *settings, const char *path);
-void		find_exit(t_settings *settings);
-void		print_map(char **map, int line_count);
+typedef struct s_mlx
+{
+	void		*mlx;
+	void		*win;
+	void		*wall_sprite;
+	void		*floor_sprite;
+	void		*collectible_sprite;
+	void		*player_sprite;
+	void		*exit_sprite;
+}				t_mlx;
+
+typedef struct s_game
+{
+	t_mlx		mlx;
+	t_settings	*settings;
+}				t_game;
+
+void			free_map(char **map, int size);
+char			**get_map(const char *path, int line_count, int *column_count);
+int				count_lines(const char *path);
+void			init_settings(t_settings *settings, const char *path);
+void			error_map(char *line, int fd, t_settings *settings, char *msg);
+void			count_all(char *line, int *count);
+void			check_count(int *count, t_settings *settings);
+void			check_map(t_settings *settings, const char *path);
+void			find_exit(t_settings *settings);
+void			print_map(char **map, int line_count);
+void			init_sprite(t_mlx *mlx, t_settings *settings);
+void			draw_map(t_mlx *mlx, t_settings *settings);
+void			init_mlx(t_settings *settings);
 
 #endif

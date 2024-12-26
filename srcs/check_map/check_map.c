479821a188e31b62d44808c65024919514959659
Loup Thevenin <loup@macbook-pro-de-loup.home>
2024-12-26 15:45:49 +0100

    more check error

--- srcs/check_map/check_map.c
+++ srcs/check_map/check_map.c
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 20:20:03 by ltheveni          #+#    #+#             */
-/*   Updated: 2024/12/24 22:32:28 by ltheveni         ###   ########.fr       */
+/*   Updated: 2024/12/26 15:43:22 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static void	wall_map(char *line, t_settings *settings, int i, int fd)
 	}
 }
 
-static int	*loop_check_map(t_settings *settings, int *count, const char *path)
+static int	loop_check_map(t_settings *settings, int *count, const char *path)
 {
 	char	*line;
 	int		fd;
@@ -48,7 +48,7 @@ static int	*loop_check_map(t_settings *settings, int *count, const char *path)
 	i = 0;
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-		return (count);
+		return (0);
 	while (1)
 	{
 		line = get_next_line(fd);
@@ -64,7 +64,21 @@ static int	*loop_check_map(t_settings *settings, int *count, const char *path)
 		i++;
 	}
 	close(fd);
-	return (count);
+	return (1);
+}
+
+static void	check_path(t_settings *settings, const char *path)
+{
+	int	len_path;
+
+	len_path = ft_strlen(path);
+	if (ft_strncmp(&path[len_path - 4], ".ber", 4) != 0)
+	{
+		free_map(settings->map, settings->line);
+		ft_putstr_fd("Error\n", 2);
+		ft_putstr_fd(".ber file", 2);
+		exit(EXIT_FAILURE);
+	}
 }
 
 void	check_map(t_settings *settings, const char *path)
@@ -74,7 +88,14 @@ void	check_map(t_settings *settings, const char *path)
 	count[0] = 0;
 	count[1] = 0;
 	count[2] = 0;
-	loop_check_map(settings, count, path);
+	check_path(settings, path);
+	if (!loop_check_map(settings, count, path))
+	{
+		free_map(settings->map, settings->line);
+		ft_putstr_fd("Error\n", 2);
+		ft_putstr_fd("File not found", 2);
+		exit(EXIT_FAILURE);
+	}
 	settings->collectible = count[2];
 	check_count(count, settings);
 }
